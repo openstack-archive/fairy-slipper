bb64d1ed7dda23c1acb26dcbcc1e0f2f93d46581
Karen Bradshaw <kbhawkey@gmail.com>
2016-04-05 18:35:57 -0400

    [WIP] Create yaml, rst files
    -Uses generated <service>*.json file from wadl files
     to create yaml and rst files.
    -Creates yaml parameter files per operation.
    -Testing, rst file generation per operation.
    -Testing, count of all parameters per service.
    
    Change-Id: Idf074d0393737fee4cd246cb3ef9f0d51c3014d7

---
+++ fairy_slipper/cmd/create_yaml_parameters.py
@@ -0,0 +1,367 @@
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import codecs
+from jinja2 import Environment
+import json
+import logging
+import os
+from os import path
+import textwrap
+import yaml
+
+
+STATUS_CODE_MAP = {
+    '200': 'Success',
+    '400': 'Bad Request',
+    '401': 'Unauthorized',
+    '403': 'Forbidden',
+    '404': 'Not Found',
+    '405': 'Method Not Allowed',
+    '409': 'Conflict',
+    '410': 'Gone',
+    '413': 'Request Entity Too Large',
+    '415': 'Unsupported Media Type',
+    '503': 'Service Unavailable',
+}
+
+
+log = logging.getLogger(__name__)
+environment = Environment()
+
+
+TMPL_API = """
+{{method['title']}}
+{% for i in range(method['title']|count) -%}
+=
+{%- endfor %}
+
+.. rest_method::  {{method['method']|upper}} {{path}}
+
+{{method['summary']}}
+
+{%- if method.description != '' %}
+{% for line in method.description.split('\n') %}
+{{line}}
+{%- endfor %}
+{%- endif %}
+
+{% for status_code, response in method['responses'].items() %}
+{%- if status_code == '200' %}
+Normal response codes: {{status_code}}
+{% endif -%}
+{%- endfor -%}
+
+
+Error response codes:
+{%- for status_code in error_codes -%}
+{{status_code}},
+{%- endfor %}
+
+
+Request Parameters
+------------------
+
+.. rest_parameters:: {{method['operationId']}}.yaml
+{% for p in request_params %}
+   - {{p}}: {{p}}
+{%- endfor %}
+
+{%- if 'application/json' in method['examples'] %}
+
+Request Example
+---------------
+
+.. literalinclude:: {{method['request_sample']}}
+   :language: javascript
+{% endif %}
+
+
+{% if response_params|count > 0 %}
+Response Parameters
+-------------------
+
+.. rest_parameters:: {{method['operationId']}}.yaml
+{% for p in response_params %}
+   - {{p}}: {{p}}
+{%- endfor %}
+{% endif %}
+
+
+{% for status_code, response in method['responses'].items() %}
+{%- if status_code == '200' %}
+Response Example
+----------------
+
+.. literalinclude:: {{response['response_sample']}}
+   :language: javascript
+
+{% endif %}
+{% endfor %}
+
+"""
+
+
+def write_rst(method_obj,
+              method_path,
+              filepath,
+              request_params,
+              response_params,
+              error_codes):
+    TMPL = environment.from_string(TMPL_API)
+    result = TMPL.render(method=method_obj,
+                         path=method_path,
+                         request_params=request_params,
+                         response_params=response_params,
+                         error_codes=error_codes)
+    log.info("Writing RST/operation %s", filepath)
+    with codecs.open(filepath,
+                     'w', "utf-8") as out_file:
+        out_file.write(result)
+
+
+def create_parameter(name, form, request, opt, ptype):
+    p = {}
+    p[name] = {}
+    if form is not '':
+        p[name]['format'] = form
+    p[name]['in'] = request
+    p[name]['required'] = opt
+    p[name]['type'] = ptype
+    return p
+
+
+def format_param(desc):
+    desc.rstrip('\n')
+    param = '  description: |\n'
+    param_wrap = textwrap.TextWrapper(
+        initial_indent=param,
+        subsequent_indent='    ')
+    if ('::\n\n' in desc) or \
+       ('.. code-block::' in desc):
+        for i, line in enumerate(desc.split('\n')):
+            if len(line) is 0:
+                param += '\n'
+            else:
+                param += '    ' + line + '\n'
+        return param.rstrip('\n')
+    else:
+        new_text = param_wrap.wrap('    ' + desc)
+        return '\n'.join(new_text)
+
+
+def main1(filename, output_dir):
+    log.info('Parsing %s' % filename)
+    swagger = json.load(open(filename))
+
+    # Parameter counter dict
+    param_count = {}
+
+    info = swagger['info']
+    version = info['version']
+    service = info['service']
+    service_path = path.join(output_dir, service)
+    full_path = path.join(service_path, version)
+    if not path.exists(service_path):
+        os.makedirs(service_path)
+    if not path.exists(full_path):
+        os.makedirs(full_path)
+
+    filename_cnt = 'param_count.txt'
+    filepath_cnt = path.join(full_path, filename_cnt)
+    count_stream = file(filepath_cnt, 'w')
+
+    for key_path, paths in swagger['paths'].items():
+        for p in paths:
+            opId = p['operationId']
+            method_req_params = p['parameters']
+
+            # list of request params
+            request_params = []
+
+            # list of response params
+            response_params = []
+
+            filename = '%s.yaml' % opId
+            filepath = path.join(full_path, filename)
+            log.info("Writing %s", filepath)
+            stream = file(filepath, 'a')
+
+            # Get the response objects
+            method_responses = p['responses']
+            for r, rval in method_responses.items():
+                # get the response header parameters
+                if 'headers' in rval:
+                    for h, val in rval['headers'].items():
+                        new_param = create_parameter(h,
+                                                     val['format'],
+                                                     str('header'),
+                                                     val['required'],
+                                                     val['type'])
+
+                        # add to response param list
+                        response_params.append(h)
+
+                        yaml.safe_dump(new_param,
+                                       stream,
+                                       allow_unicode=True,
+                                       default_flow_style=False)
+                        new_desc = format_param(val['description'])
+                        stream.write(new_desc.encode('utf8'))
+                        stream.write('\n\n')
+
+                        # add counter
+                        if h in param_count:
+                            param_count[h] += 1
+                        else:
+                            param_count[h] = 1
+
+                # get the response parameters
+                if 'schema' in rval:
+                    response_schema_name = opId + '_' + r
+                    for op, opval in swagger['definitions'].items():
+                        if op == response_schema_name:
+                            props = opval['properties']
+                            for k, val in props.items():
+                                new_param = create_parameter(k,
+                                                             val['format'],
+                                                             str("body"),
+                                                             val['required'],
+                                                             val['type'])
+
+                                # add to response param list
+                                response_params.append(k)
+
+                                yaml.safe_dump(new_param,
+                                               stream,
+                                               allow_unicode=True,
+                                               default_flow_style=False)
+                                new_desc = format_param(val['description'])
+                                stream.write(new_desc.encode('utf8'))
+                                stream.write('\n\n')
+
+                                if k in param_count:
+                                    param_count[k] += 1
+                                else:
+                                    param_count[k] = 1
+
+            # get the request parameters
+            for params in method_req_params:
+                if params['in'] == 'body':
+                    for op, opval in swagger['definitions'].items():
+                        if op == opId:
+                            props = opval['properties']
+                            for k, val in props.items():
+                                new_param = create_parameter(k,
+                                                             val['format'],
+                                                             str("body"),
+                                                             val['required'],
+                                                             val['type'])
+
+                                # add to request param list
+                                request_params.append(k)
+
+                                yaml.safe_dump(new_param,
+                                               stream,
+                                               allow_unicode=True,
+                                               default_flow_style=False)
+                                new_desc = format_param(val['description'])
+                                stream.write(new_desc.encode('utf8'))
+                                stream.write('\n\n')
+
+                                if k in param_count:
+                                    param_count[k] += 1
+                                else:
+                                    param_count[k] = 1
+
+                else:
+                    new_param = create_parameter(params['name'],
+                                                 params['format'],
+                                                 params['in'],
+                                                 params['required'],
+                                                 params['type'])
+                    # add to request param list
+                    request_params.append(params['name'])
+
+                    yaml.safe_dump(new_param,
+                                   stream,
+                                   allow_unicode=True,
+                                   default_flow_style=False)
+                    new_desc = format_param(params['description'])
+                    stream.write(new_desc.encode('utf8'))
+                    stream.write('\n\n')
+
+                    n = params['name']
+                    if n in param_count:
+                        param_count[n] += 1
+                    else:
+                        param_count[n] = 1
+
+            # write out a rst file
+            filename_rst = '%s.rst' % opId
+            filepath_rst = path.join(full_path, filename_rst)
+
+            # error responses
+            error_codes = []
+            for status_code in p['responses']:
+                if status_code > '200':
+                    error_codes.append(status_code)
+
+            write_rst(p,
+                      key_path,
+                      filepath_rst,
+                      request_params,
+                      response_params,
+                      error_codes)
+
+    # write out the param count
+    for key, cnt in param_count.items():
+        count_str = '%s:%s\n' % (key, cnt)
+        count_stream.write(count_str)
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '-v', '--verbose', action='count', default=0,
+        help="Increase verbosity (specify multiple times for more)")
+    parser.add_argument(
+        '-o', '--output-dir', action='store',
+        help="The directory where the yaml files will be output.")
+    parser.add_argument(
+        'filename',
+        help="File to convert")
+
+    args = parser.parse_args()
+
+    log_level = logging.WARNING
+    if args.verbose == 1:
+        log_level = logging.INFO
+    elif args.verbose >= 2:
+        log_level = logging.DEBUG
+
+    logging.basicConfig(
+        level=log_level,
+        format='%(asctime)s %(name)s %(levelname)s %(message)s')
+
+    filename = path.abspath(args.filename)
+
+    main1(filename, output_dir=args.output_dir)
